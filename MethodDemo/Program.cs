ec4f1901aea9473d28d93b7ae7b95b09b28fe798
YlmzBilgen <yilmazbilgenn@gmail.com>
2021-01-19 14:32:04 +0300

    Add project files.

---
+++ MethodDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba   ");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
