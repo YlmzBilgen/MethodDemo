9c679c0787db91cd516f1be0f28e7ae7f1106d55
YlmzBilgen <yilmazbilgenn@gmail.com>
2021-01-20 17:37:35 +0300

    Muster ekle, listele, sil methodları simüle edildi.

---
+++ MethodDemo/MusteriManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodDemo
+{
+    public class MusteriManager
+    {
+        private List<Musteri> musteri;
+        public List<Musteri> Musteri
+        {
+            get
+            {
+                if (musteri == null)
+                {
+                    musteri = new List<Musteri>() 
+                    {
+                        new Musteri { Id = 1, Ad = "Ali", Soyad = "Al", MusteriNo = 1000000 },
+                        new Musteri { Id = 2, Ad = "Veli", Soyad = "Bal", MusteriNo = 1000001 },
+                        new Musteri { Id = 3, Ad = "Ayşe", Soyad = "Dal", MusteriNo = 1000002 }
+                    };
+                }
+
+                return musteri;
+            } 
+            set 
+            {
+                musteri = value;
+            } 
+        }
+
+        public MusteriManager()
+        {
+        }
+        public void MusteriEkle(Musteri musteri)
+        {
+            Musteri.Add(musteri);
+        }
+
+        public List<Musteri> MusteriListele()
+        {
+            return Musteri;
+        }
+
+        public void MusteriSil(int Id) 
+        {
+            var musteri = Musteri.Find(x => x.Id == Id);
+            Musteri.Remove(musteri);
+        }
+    }
+}
--- MethodDemo/Program.cs
+++ MethodDemo/Program.cs
@@ -6,8 +6,46 @@ namespace MethodDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Merhaba");
-            Console.WriteLine("Hello World!");
+            MusteriManager manager = new MusteriManager();
+            var musteriler = manager.MusteriListele();
+ 
+            foreach (var item in musteriler)
+            {
+                Console.WriteLine($"{item.Id} - {item.Ad} - {item.Soyad} - {item.MusteriNo}");
+            }
+            Console.WriteLine("----- Musteri Listelendi -------\n");
+
+            
+
+            Musteri yeniMusteri = new Musteri()
+            {
+                Id = 4,
+                Ad = "Abbas",
+                Soyad = "Yolcu",
+                MusteriNo = 1000004
+            };
+            
+            manager.MusteriEkle(yeniMusteri);
+
+            foreach (var item in manager.MusteriListele())
+            {
+                Console.WriteLine($"{item.Id} - {item.Ad} - {item.Soyad} - {item.MusteriNo}");
+            }
+
+            Console.WriteLine("------ Musteri Eklendi ----------\n");
+
+
+
+
+            manager.MusteriSil(1);
+
+            foreach (var item in manager.MusteriListele())
+            {
+                Console.WriteLine($"{item.Id} - {item.Ad} - {item.Soyad} - {item.MusteriNo}");
+            }
+
+            Console.WriteLine("------ Musteri Silindi ----------");
+            Console.ReadKey();
         }
     }
 }
